d5a5ab2c8fa5dfb97de8a96d951f80b388850607
abirami11 <rm.abirami@gmail.com>
2015-08-14 16:39:06 -0700

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -1,21 +1,46 @@
-def bfs(s, Adj):
-	level = {s:0}
-	parent = { s:None}
-	i = 1
-	frontier = [s]
-	while frontier:
-		next = []
-		for u in frontier:
-			print u
-			for v in Adj[u]:
-				if v not in level:
-					level[v] = i
-					parent[v] = u
-					next.append(v)
-		frontier = next
-		i += 1
-	print level
+import Queue
+class TreeNode:
+	def __init__(self, key, left=None,right=None,parent = None):
+		self.leftChild = left
+		self.rightChild = right
+		self.keyValue = key
+		self.parent = parent
 
-Adj = { 'b':{'a','c'} , 'a':{'c'}, 'c':{'b'}}
-s = 'b'
-bfs(s,Adj)
+	def binary_insert(self,key):
+		if self.keyValue:
+			if key <= self.keyValue :
+				if self.hasleftChild() :
+					self.leftChild.binary_insert(key)
+				else:
+					self.leftChild = TreeNode(key)
+			else:
+				if self.hasRightChild():
+					self.rightChild.binary_insert(key)
+				else:
+					self.rightChild = TreeNode(key)
+		else:
+			self = TreeNode(key)
+
+
+	def bfs(self):
+		thisLevel = [self] 
+		while thisLevel:
+			next = []
+			for n in thisLevel:
+				print n.keyValue
+				if n.leftChild:
+					next.append(n.leftChild)
+				if n.rightChild:
+					next.append(n.rightChild)
+			thisLevel = next
+			
+
+node = TreeNode(10)
+node.binary_insert(8)
+node.binary_insert(13)
+node.binary_insert(9)
+node.binary_insert(12)
+node.binary_insert(15)
+node.binary_insert(7)
+#node.display_tree()
+node.bfs()
